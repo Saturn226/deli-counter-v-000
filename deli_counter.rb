68505a60831e2c491dad02737d605a22ad1cbde2
Ashley Muller <serenehopes@gmail.com>
2019-05-18 06:39:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 # Write your code here.
 def line(katz_deli)
-  message = "The line is currently: "
+  message = "The line is currently:"
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
     katz_deli.each_with_index do |name, index|
-      message += "#{index + 1}. #{name}"
+      message += " #{index + 1}. #{name}"
     end
   end
 end
